0e8615b97ecaab82fdcf918b0ec0f8fdc7208a47
Leonel <leonelpiter@gmail.com>
2023-11-20 20:07:59 -0300

    [cfg] - add revision y prueba3  PROD

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+const Header = () => {
+
+  return (
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4HsiKwWmxCuENy2KrJnTmZaWMZN9kkm-SC0oW7dc1Qtq30k3-zabZ54EI0_cRsDUNkwg&usqp=CAU'></img>
+  )
+}
+
+export default Header
